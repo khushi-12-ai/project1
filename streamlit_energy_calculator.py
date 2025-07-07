fa849560caf3563af74d565b7e158323a9052875
khushi-12-ai <khu1210p@gmail.com>
2025-07-07 23:45:57 +0530

    Add files via upload

--- streamlit_energy_calculator.py
+++ streamlit_energy_calculator.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-#from plotly.subplots import make_subplots
+from plotly.subplots import make_subplots
 
 # Page configuration
 st.set_page_config(
